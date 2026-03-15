b29d55449aa723083820f5858ddfa2e84f828e1e
Ted <tedbao21@outlook.com>
2026-03-15 15:06:59 +0800

    Initial Commit

---
+++ Slidesh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slidesh
+//
+//  Created by ted on 2026/3/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
